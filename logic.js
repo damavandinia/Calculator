81fb5a0850354aa312efa670bbb0eb56c79a9c9a
sobhan <damavandisobhan@gmail.com>
2022-10-31 18:47:25 +0330

    edit

---
+++ logic.js
@@ -0,0 +1,168 @@
+
+var lastAction = 'clean';
+var calItems = [];
+var currentNumber = '';
+
+function handleNumBtn(btnType){
+
+    if (currentNumber.length >= 10){return;}
+
+    if (currentNumber === '' && btnType === '.'){
+        currentNumber = '0.';
+    }else {
+        currentNumber += btnType;
+    }
+
+    if (lastAction === 'number'){
+        calItems.splice(-1);
+    }
+
+    calItems.push(currentNumber);
+
+    console.log(calItems);
+
+    lastAction = 'number';
+
+    printResult();
+}
+
+function buttonAction(btnType){
+
+    switch (btnType){
+
+        case 'equal':
+            currentNumber = '';
+            calculate();
+            return;
+
+        case 'ac':
+            calItems = [];
+            currentNumber = '';
+            lastAction = 'clean';
+
+            console.log(calItems);
+
+            document.getElementById("logicTxt").style.fontSize = 50+'px';
+            printResult();
+            return;
+
+        case 'delete':
+
+            if (lastAction === 'clean'){return;}
+
+            if (lastAction === 'logic'){
+
+                calItems.splice(-1);
+                lastAction = 'number';
+
+            }else if (lastAction === 'number'){
+
+                const lastNum = (calItems[calItems.length - 1]).toString();
+                const newNum = lastNum.substring(0, lastNum.length - 1);
+
+                calItems.splice(-1);
+                if (newNum === ''){
+                    lastAction = 'logic';
+                }else {
+                    calItems.push(parseFloat(newNum));
+                }
+
+                currentNumber = newNum;
+            }
+
+            if (calItems.length === 0){
+                lastAction = 'clean';
+            }
+
+            console.log(calItems);
+
+            printResult();
+            return;
+
+        case 'percent':
+
+            if (lastAction !== 'number'){return;}
+
+            currentNumber = currentNumber/100;
+
+            calItems.splice(-1);
+            calItems.push(parseFloat(currentNumber));
+
+            console.log(calItems);
+
+            printResult();
+            return;
+
+    }
+
+    if (calItems.length === 0 && lastAction === 'clean' || lastAction === 'logic'){return;}
+
+    currentNumber = '';
+    calItems.push(btnType);
+
+    console.log(calItems);
+
+    // lastAction = btnType !== 'delete' ? 'logic' : 'clean';
+
+    lastAction = 'logic';
+
+    printResult();
+}
+
+function calculate(){
+
+    if (lastAction === 'logic' || lastAction === 'clean'){return;}
+
+    const logicPhrase = getLogicPhrase();
+
+    let result = evil(logicPhrase);
+    console.log(result);
+
+    currentNumber = result.toString();
+    calItems = [currentNumber];
+
+    document.getElementById("resultTxt").style.opacity = '1';
+    document.getElementById("resultTxt").innerText = '= ' + result.toString();
+    document.getElementById("resultTxt").classList.add("calResultTxt");
+    document.getElementById("logicTxt").classList.remove("calResultTxt");
+
+    console.log(calItems);
+}
+
+function evil(fn) {
+    return new Function('return ' + fn)();
+}
+
+function getLogicPhrase(){
+
+    let logicPhrase = '';
+
+    for (let item of calItems){
+
+        logicPhrase += item;
+    }
+
+    console.log(logicPhrase);
+
+    return logicPhrase;
+}
+
+function printResult(){
+
+    document.getElementById("resultTxt").style.opacity = '0';
+
+    let finalPhrase = getLogicPhrase().replace('/' , '÷');
+    finalPhrase = finalPhrase.replace('*' , '×');
+
+    if (finalPhrase === ''){finalPhrase = 0;}
+
+    document.getElementById("logicTxt").innerText = finalPhrase;
+    document.getElementById("logicTxt").classList.add("calResultTxt");
+    document.getElementById("resultTxt").classList.remove("calResultTxt");
+
+    let textLength = finalPhrase.length;
+
+    if (textLength > 11){
+        document.getElementById("logicTxt").style.fontSize = 50-textLength+'px';
+    }
+}
